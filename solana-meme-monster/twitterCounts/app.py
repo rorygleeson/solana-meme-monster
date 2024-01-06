aeecb8ad8a4f01243af815b80a1eb2296e7bdff2
EC2 Default User <ec2-user@ip-172-31-35-165.ap-southeast-2.compute.internal>
2024-01-06 23:04:47 +0000

    fix api gw error

--- solana-meme-monster/twitterCounts/app.py
+++ solana-meme-monster/twitterCounts/app.py
@@ -1,40 +1,5 @@
 import json
-import tweepy
 # import requests
-
-def get_twitter_keys():
-    """Retrieve secrets from Parameter Store."""
-    # Create our SSM Client.
-    aws_client = boto3.client('ssm')
-
-    # Get our keys from Parameter Store.
-    parameters = aws_client.get_parameters(
-        Names=[
-            'twitter_api_key',
-            'twitter_api_secret',
-            'twitter_access_token',
-            'twitter_access_secret'
-        ],
-        WithDecryption=True
-    )
-
-    # Convert list of parameters into simpler dict.
-    keys = {}
-    for parameter in parameters['Parameters']:
-        keys[parameter['Name']] = parameter['Value']
-
-    return keys
-
-
-
-def get_tweet():
-    """Creates our tweet."""
-
-  
-    tweet = "WTF5673"
-    return tweet
-
-
 
 
 def lambda_handler(event, context):
@@ -68,22 +33,11 @@ def lambda_handler(event, context):
     #     raise e
 
 
-    keys = get_twitter_keys()
-
-    client = tweepy.Client(
-        consumer_key=keys.get('twitter_api_key'),
-        consumer_secret=keys.get('twitter_api_secret'),
-        access_token=keys.get('twitter_access_token'),
-        access_token_secret=keys.get('twitter_access_secret')
-    )
-
-    tweet = get_tweet()
-    client.create_tweet(text=tweet)
-
+  
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Tweet Sent? BONK token mentions in last hour = 34219",
+            "message": "Tweet Sent BONK token mentions in last hour = 34219",
             # "location": ip.text.replace("\n", "")
         }),
     }
