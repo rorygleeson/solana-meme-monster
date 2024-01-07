d2ca446dbeec27444c7b3d30f1072f1d5abdfa17
EC2 Default User <ec2-user@ip-172-31-35-165.ap-southeast-2.compute.internal>
2024-01-07 08:09:28 +0000

    crawler updated 2

--- solana-meme-monster/crawler/app.py
+++ solana-meme-monster/crawler/app.py
@@ -1,12 +1,67 @@
 import json
 # import requests
+import tweepy
+import boto3
+
+
+def get_twitter_keys():
+    """Retrieve secrets from Parameter Store."""
+    # Create our SSM Client.
+    aws_client = boto3.client('ssm')
+
+    # Get our keys from Parameter Store.
+    parameters = aws_client.get_parameters(
+        Names=[
+            'twitter_bearer_code'
+        ],
+        WithDecryption=True
+    )
+
+    # Convert list of parameters into simpler dict.
+    keys = {}
+    for parameter in parameters['Parameters']:
+        keys[parameter['Name']] = parameter['Value']
+
+    return keys
+    
 
 
 def lambda_handler(event, context):
 
+
+    keys = get_twitter_keys()
+
+    client = tweepy.Client(
+        bearer_token=keys.get('twitter_bearer_code')
+    )
+
+   
+    """
+    If you don't understand search queries, there is an excellent introduction to it here: 
+    https://github.com/twitterdev/getting-started-with-the-twitter-api-v2-for-academic-research/blob/main/modules/5-how-to-write-search-queries.md
+    """
+   
+    # Get tweets that contain the hashtag #petday
+    # -is:retweet means I don't want retweets
+    # lang:en is asking for the tweets to be in english
+    query = '#petday -is:retweet lang:en'
+    tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100)
+    
+    
+    """
+    What context_annotations are: 
+    https://developer.twitter.com/en/docs/twitter-api/annotations/overview
+    """
+   
+    for tweet in tweets.data:
+        print(tweet.text)
+        if len(tweet.context_annotations) > 0:
+            print(tweet.context_annotations)
+   
+   
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "crawlStart": "GO",
+            "crawlStart": "Go CrawlerFunction",
         }),
     }
